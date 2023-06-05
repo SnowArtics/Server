8193fb3cd5d7318e7b2d5a556704f81295e5a4b1
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-06-06 05:15:09 +0900

    FEAT : Create Stomp Allocator

--- ServerCore/Allocator.h
+++ ServerCore/Allocator.h
@@ -11,3 +11,17 @@ public:
 	static void		Release(void* ptr);
 };
 
+
+/*-----------------------------------------
+|			StompAllocator
+-----------------------------------------*/
+//메모리 오류를 잡는데 쓴다.
+
+class StompAllocator {
+
+	enum { PAGE_SIZE = 0x1000 };
+
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+};
