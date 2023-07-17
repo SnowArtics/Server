5c1e3d294d76eb279ee93654f3e536c52cf4ec96
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-07-18 03:18:50 +0900

    FEAT : Create Job Queue
    Create Job Queue using template
    (07.17)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,17 +8,56 @@
 #include "ClientPacketHandler.h"
 #include <tchar.h>
 #include "Protocol.pb.h"
+#include "Job.h"
 #include "Room.h"
+
+void HealByValue(int64 target, int32 value)
+{
+	cout << target << "한테 힐" << value << "만큼 줌" << endl;
+}
+
+class Knight
+{
+public:
+	void HealMe(int32 value)
+	{
+		cout << "HealMe! " << value << endl;
+	}
+};
 
 int main()
 {
+	auto tup = std::tuple<int32, int32>(1, 2);
+	auto val0 = std::get<0>(tup);
+	auto val1 = std::get<1>(tup);
+
+	auto s = gen_seq<3>();
+	// gen_seq<3>
+	// : gen_seq<2, 2>
+	// : gen_seq<1, 1, 2>
+	// : gen_seq<0, 0, 1, 2>
+	// : seq<0, 1, 2>
+
+	// TEST JOB
+	{
+		FuncJob<void, int64, int32> job(HealByValue, 100, 10);
+		job.Execute();
+	}
+	{
+		Knight k1;
+		MemberJob job2(&k1, &Knight::HealMe, 10);
+		job2.Execute();
+	}
+
+	// JOB
+
 	ClientPacketHandler::Init();
 
 	ServerServiceRef service = MakeShared<ServerService>(
 		NetAddress(L"127.0.0.1", 7777),
 		MakeShared<IocpCore>(),
 		MakeShared<GameSession>, // TODO : SessionManager 등
-		1);
+		100);
 
 	ASSERT_CRASH(service->Start());
 
