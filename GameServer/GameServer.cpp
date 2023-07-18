30dec7c21208ace0b77d6d929e6e6774675baeb0
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-07-18 23:09:18 +0900

    FEAT : Create Job Timer (07.18)
    Create Job Timer (07.18)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -26,6 +26,9 @@ void DoWorkerJob(ServerServiceRef& service)
 		// 네트워크 입출력 처리 -> 인게임 로직까지 (패킷 핸들러에 의해)
 		service->GetIocpCore()->Dispatch(10);
 
+		// 예약된 일감 처리
+		ThreadManager::DistributeReservedJobs();
+
 		// 글로벌 큐
 		ThreadManager::DoGlobalQueueWork();
 	}
@@ -33,13 +36,17 @@ void DoWorkerJob(ServerServiceRef& service)
 
 int main()
 {
+	GRoom->DoTimer(1000, [] { cout << "Hello 1000" << endl; });
+	GRoom->DoTimer(2000, [] { cout << "Hello 2000" << endl; });
+	GRoom->DoTimer(3000, [] { cout << "Hello 3000" << endl; });
+
 	ClientPacketHandler::Init();
 
 	ServerServiceRef service = MakeShared<ServerService>(
 		NetAddress(L"127.0.0.1", 7777),
 		MakeShared<IocpCore>(),
 		MakeShared<GameSession>, // TODO : SessionManager 등
-		100);
+		1);
 
 	ASSERT_CRASH(service->Start());
 
